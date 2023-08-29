df7083705718a3997a832554dade9c8a3dc95530
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2023-08-29 20:05:49 +0300

    trigger ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,7 @@ export default async function Home() {
 
   console.log("itemDetails", itemDetails);
 
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
