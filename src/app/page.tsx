810138638f6a9aa795d60c47053c6b54f1c121a9
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2023-08-29 20:02:47 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
 
-export default function Home() {
+const baseUrl =
+  "https://collect.dialectapi.to" + "/api/v1/nft-collection-items";
+
+async function fetchItemDetails(itemId: string): Promise<any | null> {
+  console.log("fetchItemDetails", itemId);
+  const response = await fetch(`${baseUrl}/${itemId}`);
+  if (!response.ok) {
+    console.error(response);
+    return null;
+  }
+  return response.json();
+}
+
+export default async function Home() {
+  const itemDetails = await fetchItemDetails(
+    "40e5aad0-259c-41d3-abcd-7b78940a20f6"
+  );
+
+  console.log("itemDetails", itemDetails);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {Math.random()}
+          {Math.random()}&nbsp;
+          {itemDetails?.item?.name}
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
